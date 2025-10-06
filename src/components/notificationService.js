5c74310f72d9288ee6982af5f0926197c7bd44ad
Your Name <sandrine.celenie@gmail.com>
2025-10-06 13:39:27 +0700

    Add missing notificationService file

---
+++ src/components/notificationService.js
@@ -0,0 +1,10 @@
+// src/components/notificationService.js
+export const sendNotificationToSam = (message) => {
+  console.log('Notification to Sam:', message);
+  // Add your notification logic here
+};
+
+export const sendClientNotification = (message) => {
+  console.log('Client notification:', message);
+  // Add your client notification logic here
+};
